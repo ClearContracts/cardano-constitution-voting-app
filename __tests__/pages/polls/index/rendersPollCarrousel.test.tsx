2da40506c7cde8c4b782f06224c1e0c79e63ba9f
Matthew Laux <mlaux6@outlook.com>
2024-11-25 20:04:03 -0600

    Fix ViewPoll props

--- __tests__/pages/polls/index/rendersPollCarrousel.test.tsx
+++ __tests__/pages/polls/index/rendersPollCarrousel.test.tsx
@@ -27,7 +27,84 @@ test('renders poll carrousel', async () => {
         },
       }}
     >
-      <ViewPoll />
+      <ViewPoll
+        poll={{
+          id: '1',
+          name: 'Poll #1',
+          hashedText:
+            '1dda86a28da3715e618a1605f81c7a3121ce767cdc9bd0d46beec0bef40e8125',
+          link: 'https://2024constitutionalconsultation.docs.intersectmbo.org/cardanos-constitution/draft-cardano-constitution',
+          status: 'pending',
+          summary_tx_id: null,
+          is_archived: false,
+        }}
+        representatives={[
+          {
+            id: '1',
+            is_convention_organizer: true,
+            is_delegate: false,
+            is_alternate: false,
+            workshop_id: '1',
+            name: 'Test CO',
+            email: 'email@email.com',
+            wallet_address: 'coStakeAddress',
+          },
+          {
+            id: '7',
+            is_convention_organizer: false,
+            is_delegate: true,
+            is_alternate: false,
+            workshop_id: '2',
+            name: 'Test Delegate',
+            email: 'delagate@email.com',
+            wallet_address: 'delegateStakeAddress',
+          },
+          {
+            id: '8',
+            is_convention_organizer: false,
+            is_delegate: false,
+            is_alternate: true,
+            workshop_id: '2',
+            name: 'Test Alternate',
+            email: 'alternate@email.com',
+            wallet_address: 'alternateStakeAddress',
+          },
+        ]}
+        workshops={[
+          {
+            id: '1',
+            name: 'Convention Organizer',
+            delegate_id: null,
+            alternate_id: null,
+            active_voter_id: null,
+          },
+          {
+            id: '2',
+            name: 'Argentina, Buenos Aires 1',
+            delegate_id: '7',
+            alternate_id: '8',
+            active_voter_id: '7',
+          },
+        ]}
+        pollResultsSSR={{
+          yes: [],
+          no: [],
+          abstain: [],
+        }}
+        polls={[
+          {
+            id: '1',
+            name: 'Poll #1',
+            hashedText:
+              '1dda86a28da3715e618a1605f81c7a3121ce767cdc9bd0d46beec0bef40e8125',
+            link: 'https://2024constitutionalconsultation.docs.intersectmbo.org/cardanos-constitution/draft-cardano-constitution',
+            status: 'pending',
+            summary_tx_id: null,
+            is_archived: false,
+          },
+        ]}
+        workshopActiveVoterId=""
+      />
     </SessionProvider>,
   );
 
