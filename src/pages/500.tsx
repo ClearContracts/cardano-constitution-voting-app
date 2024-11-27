a0ca55a5db4372067904a1349b2eebf21fb09325
Matthew Laux <mlaux6@outlook.com>
2024-11-27 12:40:53 -0600

    Add test 500 page

---
+++ src/pages/500.tsx
@@ -0,0 +1,8 @@
+export default function Custom500() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>500 - Server-side error occurred</h1>
+      <p>We’re sorry, but something went wrong. Please try again later.</p>
+    </div>
+  );
+}
