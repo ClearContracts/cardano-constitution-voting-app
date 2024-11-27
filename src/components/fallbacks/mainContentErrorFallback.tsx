3a9e6e69e8dff4bbcf70bbc19e1f6688cfaf305b
Matthew Laux <mlaux6@outlook.com>
2024-11-27 12:11:03 -0600

    Add react error boundary

---
+++ src/components/fallbacks/mainContentErrorFallback.tsx
@@ -0,0 +1,53 @@
+import gradient from '@/img/constitution-gradient.png';
+import texture from '@/img/Pattern.png';
+import { Box, Button } from '@mui/material';
+import Typography from '@mui/material/Typography';
+import type { FallbackProps } from 'react-error-boundary';
+
+import styles from '@/styles/Layout.module.css';
+
+// Refer to https://github.com/bvaughn/react-error-boundary/issues/57 for TS props
+// Refer to https://www.npmjs.com/package/react-error-boundary to add logging
+export function MainContentErrorFallback({
+  resetErrorBoundary,
+}: FallbackProps): JSX.Element {
+  return (
+    <>
+      <Box
+        sx={{
+          background: 'rgb(13,26,61)',
+          backgroundImage: `url(${gradient.src})`,
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover',
+        }}
+        className={styles.background}
+      />
+      <Box
+        sx={{
+          backgroundImage: `url(${texture.src})`,
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+        }}
+        className={styles.backgroundTexture}
+        mt="75px"
+      />
+      <Box
+        display="flex"
+        flexDirection="column"
+        gap={2}
+        alignItems="center"
+        justifyContent="center"
+        minHeight="100vh"
+        textAlign="center"
+      >
+        <Typography sx={{ color: 'white' }} variant="h4">
+          Failed to load content. Please return to the home page.
+        </Typography>
+        <Button variant="contained" onClick={resetErrorBoundary}>
+          Return to home page
+        </Button>
+      </Box>
+    </>
+  );
+}
