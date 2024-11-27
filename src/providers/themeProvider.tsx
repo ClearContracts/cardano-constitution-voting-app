8fe5fed42e369cac7dff43c463037b924da4892e
Justin Schreiner <jschreiner22@gmail.com>
2024-11-27 16:29:10 -0600

    representatives table

--- src/providers/themeProvider.tsx
+++ src/providers/themeProvider.tsx
@@ -17,7 +17,7 @@ export function ColorModeProvider({
 }): JSX.Element {
   const theme = createTheme({
     shape: {
-      borderRadius: 8,
+      borderRadius: 4,
     },
     palette: {
       // palette values for light mode
