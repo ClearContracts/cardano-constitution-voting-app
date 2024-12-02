e56395df8ed6c35c7b67bec35b9d861f2588cb7f
Justin Schreiner <jschreiner22@gmail.com>
2024-12-02 14:02:38 -0300

    delegate signature transactions

--- src/pages/api/getSummaryTxMetadata/[pollId].ts
+++ src/pages/api/getSummaryTxMetadata/[pollId].ts
@@ -127,7 +127,7 @@ export default async function getSummaryTxMetadata(
       noString,
       abstainString,
       totalString,
-      'Transaction Hashes:',
+      'Delegate Signature Transaction Hashes:',
       ...txIds,
     ];
 
